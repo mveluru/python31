571eb2a7656766943c88577ab458d250eab3d479
mveluru <mveluru@gmail.com>
2024-05-03 10:11:16 -0500

    Init

--- src/com/brite/list/StringList.py
+++ src/com/brite/list/StringList.py
@@ -1,4 +1,16 @@
 phonesList = ['apple', 'samsung', 'oneplus', 'nokia']
+
+
 phoneList_iter = iter(phonesList)
 for item in phoneList_iter:
-    print(item)
+    print(item, end=' ')
+
+print('\n***sorted***')
+sortedlist = sorted(phonesList)
+print(sortedlist)
+
+print('\n***reverse order***')
+print(sortedlist[::-1])
+
+print('\n***specific position***')
+print(sortedlist[0])

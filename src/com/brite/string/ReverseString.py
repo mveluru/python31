14da2e6f8e50d83440c59f15fd2293695c782906
mveluru <mveluru@gmail.com>
2024-05-03 11:37:05 -0500

    Init

---
+++ src/com/brite/string/ReverseString.py
@@ -0,0 +1,12 @@
+name = 'JOHNSMITH'
+print(name)
+
+print('***reverse the given string***')
+
+rerversestring = name[::-1]
+print(rerversestring)
+
+## compare strings
+
+
+
---
+++ src/com/brite/string/Stringpalindrom.py
@@ -0,0 +1,11 @@
+name = 'JOHNSMITH'
+print(name)
+
+revname = name[::-1]
+print(revname)
+
+print('comparing two strings: ')
+if name == revname :
+    print(' palindrome string')
+else:
+    print('not a palindrome')
